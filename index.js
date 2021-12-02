219ae1880d3ee6dab9048a56d8b8a496bde4d426
Arrigoni Alessandro <arrigoni.alessandro@osdbsesto>
2021-12-02 11:45:27 +0100

    nome

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var express = require ("express") //dichiarare utilizzo del node modules 
+const { request, response } = require("express");
+var express = require("express"); //dichiarare utilizzo del node modules
 var apiServer = express();
 
 /* console.log("funziona");
@@ -8,18 +9,22 @@ console.log(a+b); */
 
 var port = 3000;
 var host = "localhost";
-apiServer.listen(port,host,()=>{ //attiva il processo web server in ascolto
-console.log("server running at http://%s:%d",host,port )
+apiServer.listen(port, host, () => {
+  //attiva il processo web server in ascolto
+  console.log("server running at http://%s:%d", host, port);
 });
 
-apiServer.get("/", (request, response)=>{
-console.log("sono in get /");
-response.send("<h1>ciao sono il client sei in home</h1> "); //risposta 
-})
+apiServer.get("/", (request, response) => {
+  console.log("sono in get /");
+  response.send("<h1>ciao sono il client sei in home</h1> "); //risposta
+});
 
-apiServer.get("/nome", (request, response)=>{
-console.log("richiesta nome ");
-response.send("<h1>Alessandro Arrigoni</h1>");
+apiServer.get("/nome", (request, response) => {
+  console.log("richiesta nome ");
+  response.send("<h1>Alessandro Arrigoni</h1>");
+});
 
-
-})
+apiServer.get("/mioNome", (request, response) => {
+  console.log(request.query.nome);
+  response.send("<h1>il mio nome è:</h1>");
+});
