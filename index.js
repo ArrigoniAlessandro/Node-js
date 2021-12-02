793dac860d9434f63becda8b059d522fa27adbce
Arrigoni Alessandro <arrigoni.alessandro@osdbsesto>
2021-12-02 11:22:58 +0100

    Api server GetNome

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 var express = require ("express") //dichiarare utilizzo del node modules 
 var apiServer = express();
 
-
-
-
 /* console.log("funziona");
 var a = 5; 
 var b = "3";
 console.log(a+b); */
+
 var port = 3000;
 var host = "localhost";
 apiServer.listen(port,host,()=>{ //attiva il processo web server in ascolto
 console.log("server running at http://%s:%d",host,port )
 });
+
 apiServer.get("/", (request, response)=>{
-console.log("sono in get /", request);
+console.log("sono in get /");
 response.send("<h1>ciao sono il client sei in home</h1> "); //risposta 
+})
+
+apiServer.get("/nome", (request, response)=>{
+console.log("richiesta nome ");
+response.send("<h1>Alessandro Arrigoni</h1>");
 
 
 })
